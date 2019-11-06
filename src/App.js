abd6836491ccf8a140ba144fc9cbe0e9c9b95863
Mirtles <mirte.post@yahoo.co.uk>
2019-11-06 17:08:47 +0100

    Add button to watch component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 
 import Test from './components/Test'
+import Watch from './components/Watch'
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Watch />
       <Test />
     </div>
   );
---
+++ src/components/Watch.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Watch() {
+
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    console.log('effect runs');
+    const interval = setInterval(() => console.log('tick'), 1000);
+
+    return () => clearInterval(interval);
+  }, [])
+
+  return (
+    <div>
+      {!isOn && (
+        <button type="button" onClick={() => setIsOn(true)}>
+          Start
+        </button>
+      )}
+
+      {isOn && (
+        <button type="button" onClick={() => setIsOn(false)}>
+          Stop
+        </button>
+      )}
+    </div>
+  );
+}
